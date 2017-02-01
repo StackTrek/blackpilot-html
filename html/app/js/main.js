c612894aa9d26ac3736f96b90be7068fa4b9944d
Ian Pansensoy <ipansensoy@gmail.com>
2017-02-01 13:41:17 +0800

    done partial panels

--- html/app/js/main.js
+++ html/app/js/main.js
@@ -1,6 +1,16 @@
 
 "use strict";
 $(function() {
-  $('.date-select').datepicker({
-});
+  $('.date-select').datepicker({});
+  $('.control-button-trigger').each(function(){
+      $(this).on('click',function(e){
+        e.preventDefault();
+        $('.control-button-trigger').removeClass('active');
+        $(this).addClass('active');
+        var elHref = $(this).attr('href');
+        $('.control-properties').removeClass('open-this');
+        $(elHref).addClass('open-this');
+
+      });
+  });
 });
